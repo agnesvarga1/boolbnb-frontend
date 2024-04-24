e4905a7b5b74f94016871231dc6492f446723afb
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-04-24 15:58:37 +0200

    feat: apartment card and apartment cards component (beta)

---
+++ src/components/ApartmentCards.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  name: "ApartmentCards",
+  components: {
+
+  },
+  data() {
+    return {
+        apartments:[],
+    };
+  },
+  methods: {
+
+  },
+  mounted() {
+    axios.get('/api/apartments')
+      .then(response => {
+        console.log(response)
+        this.apartments = response.data;
+      })
+      .catch(error => {
+        console.error('There was an error fetching the apartments:', error);
+      });
+  },
+};
+</script>
+
+<template>
+<router-link :to="{ name: 'Appartament' }" class="nav-link">
+
+    <div class="card h-100" @click="redirectToApartment(apartment.slug)">
+
+        <!-- <img :src="apartment.cover_image" class="card-img-top" :alt="apartment.title"> -->
+        
+        <div class="card-body d-flex flex-column">
+            <h5 class="card-title">{{ apartment.title }}</h5>
+            <p class="card-text mb-2">{{ apartment.full_address }}</p>
+            <h6 class="card-title">{{ apartment.price }}€/notte</h6>
+            <button @click="redirectToApartment(apartment.slug)" class="btn btn-primary">
+                Vedi di più
+            </button>
+        </div>
+
+    </div>
+
+</router-link>
+</template>
+
+<style>
+
+</style>
--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,12 +1,26 @@
 <script>
+import axios from 'axios';
+
 export default {
   name: "Homepage",
   components: {},
   data() {
-    return {};
+    return {
+      apartments:[]
+    };
   },
   methods: {},
-  mounted() {},
+  mounted() {
+    axios.get('/api/apartments')
+      .then(response => {
+        console.log(response)
+        this.apartments = response.data;
+      })
+      .catch(error => {
+        console.error('There was an error fetching the apartments:', error);
+      });
+  }
+,
 };
 </script>
 
@@ -29,7 +43,7 @@ export default {
         <h2 class="fw-bold">Foresty Appartments</h2>
       </div>
       <div class="py-2 mt-3">
-        <a class="btn btn-primary" href="#" role="button">Get Details!</a>
+        <a class="btn btn-green" href="#" role="button">Get Details!</a>
       </div>
     </div>
   </div>
@@ -59,42 +73,38 @@ export default {
     </div>
   </section>
 
+  <!-- SECTION Homepage body -->
   <section class="container my-5">
+
     <h2 class="mb-4">Featured Appartments</h2>
+
     <div class="row">
-      <!-- Card (Fare v-for o foreach (laravel o vue)) -->
-      <div class="row">
+        <!-- Card (Fare v-for o foreach (laravel o vue)) -->
+        <div class="row">
         <router-link :to="{ name: 'Appartament' }" class="nav-link">
-          <!-- Appartment -->
-
-          <!-- @foreach ($apartments as $apartment) -->
-
-          <div class="col-md-4 mb-4">
-            <div class="card h-100" onclick="window.location='';">
-              <!-- <img src="{{ $apartment->cover_image }}" class="card-img-top" alt="{{ $apartment->slug }}"> -->
-
+          <div v-for="apartment in apartments" :key="apartment.id" class="col-md-4 mb-4">
+            <div class="card h-100" @click="redirectToApartment(apartment.slug)">
+              <img :src="apartment.cover_image" class="card-img-top" :alt="apartment.title">
               <div class="card-body d-flex flex-column">
-                <h5 class="card-title">
-                  <!-- {{ $apartment->title }} -->
-                </h5>
-
-                <p class="card-text mb-2">
-                  <!-- {{ $apartment->full_address }} -->
-                </p>
-
-                <h6 class="card-title">
-                  <!-- {{ $apartment->price }}€/notte -->
-                </h6>
-
-                <!-- Redirect alla show -->
+                <h5 class="card-title">{{ apartment.title }}</h5>
+                <p class="card-text mb-2">{{ apartment.full_address }}</p>
+                <h6 class="card-title">{{ apartment.price }}€/notte</h6>
+                <button @click="redirectToApartment(apartment.slug)" class="btn btn-primary">
+                  Vedi di più
+                </button>
               </div>
-            </div>
           </div>
-        </router-link>
-        <!-- @endforeach -->
       </div>
+      </router-link>
+    </div>
+
+
+        
     </div>
   </section>
+
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+
+</style>
