ca9b7bd0798a382c39607d667a3149e9886c8595
agnesvarga1 <agikolyok@gmail.com>
2024-04-24 15:22:28 +0200

    colors in vars

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import { router } from "./router";
-//import './style.css'
+
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
-import '@fortawesome/fontawesome-free/css/all.css'
-
+import "@fortawesome/fontawesome-free/css/all.css";
+import "./styles/general.scss";
 createApp(App).use(router).mount("#app");
