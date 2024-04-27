7c721a39abe92e75db812f00f5647935f94e15c3
michelearcopinto <guestphantom1@gmail.com>
2024-04-27 19:17:27 +0200

    fix: fixed changing apartments on map

--- src/components/TomTomMap.vue
+++ src/components/TomTomMap.vue
@@ -1,6 +1,7 @@
 <script>
 import tt from "@tomtom-international/web-sdk-maps";
 import { store } from "../store.js";
+import { toRaw } from "vue";
 
 export default {
   name: "TomTomMap",
@@ -12,17 +13,6 @@ export default {
   },
   methods: {
     inzializeMap() {
-      store.map = tt.map({
-        key: import.meta.env.VITE_TOMTOM_API_KEY,
-        container: "tomtom-map",
-        center: [13, 41],
-        zoom: 4,
-      });
-
-      store.map.addControl(new tt.FullscreenControl());
-      store.map.addControl(new tt.NavigationControl());
-    },
-    createMarkers() {
       const map = tt.map({
         key: import.meta.env.VITE_TOMTOM_API_KEY,
         container: "tomtom-map",
@@ -33,6 +23,14 @@ export default {
       map.addControl(new tt.FullscreenControl());
       map.addControl(new tt.NavigationControl());
 
+      map.on("load", () => {
+        store.map = map;
+        this.createMarkers();
+      });
+    },
+    createMarkers() {
+      const map = toRaw(store.map);
+
       var markersOnTheMap = {};
       var eventListenersAdded = false;
 
@@ -41,6 +39,7 @@ export default {
         features: this.propApartments.map(function (apartment) {
           return {
             type: "Feature",
+            id: apartment.id,
             geometry: {
               type: "Point",
               coordinates: [apartment.longitude, apartment.latitude],
@@ -54,6 +53,12 @@ export default {
         }),
       };
 
+      if (map.getSource("point-source")) {
+        map.removeLayer("cluster-count");
+        map.removeLayer("clusters");
+        map.removeSource("point-source");
+      }
+
       function refreshMarkers() {
         Object.keys(markersOnTheMap).forEach(function (id) {
           markersOnTheMap[id].remove();
@@ -62,7 +67,8 @@ export default {
 
         map.querySourceFeatures("point-source").forEach(function (feature) {
           if (feature.properties && !feature.properties.cluster) {
-            var id = parseInt(feature.properties.id, 10);
+            var id = feature.properties.id;
+            console.log(id);
             if (!markersOnTheMap[id]) {
               var newMarker = new tt.Marker().setLngLat(
                 feature.geometry.coordinates
@@ -75,101 +81,101 @@ export default {
                   offset: 75 / 2,
                   anchor: "bottom",
                 }).setHTML(`
-                  <b><h5>${feature.properties.name}</h5></b>
-                  <span>${feature.properties.full_address}</span>
-                  `)
+                <b><h5>${feature.properties.name}</h5></b>
+                <span>${feature.properties.full_address}</span>
+                `)
               );
               markersOnTheMap[id] = newMarker;
+              console.log(Object.keys(markersOnTheMap));
             }
           }
         });
       }
 
-      map.on("load", function () {
-        map.addSource("point-source", {
-          type: "geojson",
-          data: geoJson,
-          cluster: true,
-          clusterMaxZoom: 14,
-          clusterRadius: 50,
+      map.addSource("point-source", {
+        type: "geojson",
+        data: geoJson,
+        cluster: true,
+        clusterMaxZoom: 14,
+        clusterRadius: 50,
+      });
+      map.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "point-source",
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#EC619F",
+            4,
+            "#008D8D",
+            7,
+            "#004B7F",
+          ],
+          "circle-radius": ["step", ["get", "point_count"], 15, 4, 20, 7, 25],
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "white",
+          "circle-stroke-opacity": 1,
+        },
+      });
+      map.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "point-source",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": "{point_count_abbreviated}",
+          "text-size": 16,
+        },
+        paint: {
+          "text-color": "white",
+        },
+      });
+      map.on("data", function (e) {
+        if (
+          e.sourceId !== "point-source" ||
+          !map.getSource("point-source") ||
+          !map.getSource("point-source").loaded()
+        ) {
+          return;
+        }
+        refreshMarkers();
+        if (!eventListenersAdded) {
+          console.log("markers");
+          map.on("move", refreshMarkers);
+          map.on("moveend", refreshMarkers);
+          eventListenersAdded = true;
+        }
+      });
+      map.on("click", "clusters", function (e) {
+        var features = map.queryRenderedFeatures(e.point, {
+          layers: ["clusters"],
         });
-        map.addLayer({
-          id: "clusters",
-          type: "circle",
-          source: "point-source",
-          filter: ["has", "point_count"],
-          paint: {
-            "circle-color": [
-              "step",
-              ["get", "point_count"],
-              "#EC619F",
-              4,
-              "#008D8D",
-              7,
-              "#004B7F",
-            ],
-            "circle-radius": ["step", ["get", "point_count"], 15, 4, 20, 7, 25],
-            "circle-stroke-width": 1,
-            "circle-stroke-color": "white",
-            "circle-stroke-opacity": 1,
-          },
-        });
-        map.addLayer({
-          id: "cluster-count",
-          type: "symbol",
-          source: "point-source",
-          filter: ["has", "point_count"],
-          layout: {
-            "text-field": "{point_count_abbreviated}",
-            "text-size": 16,
-          },
-          paint: {
-            "text-color": "white",
-          },
-        });
-        map.on("data", function (e) {
-          if (
-            e.sourceId !== "point-source" ||
-            !map.getSource("point-source").loaded()
-          ) {
-            return;
-          }
-          refreshMarkers();
-          if (!eventListenersAdded) {
-            map.on("move", refreshMarkers);
-            map.on("moveend", refreshMarkers);
-            eventListenersAdded = true;
-          }
-        });
-        map.on("click", "clusters", function (e) {
-          var features = map.queryRenderedFeatures(e.point, {
-            layers: ["clusters"],
-          });
-          var clusterId = features[0].properties.cluster_id;
-          map
-            .getSource("point-source")
-            .getClusterExpansionZoom(clusterId, function (err, zoom) {
-              if (err) {
-                return;
-              }
-              map.easeTo({
-                center: features[0].geometry.coordinates,
-                zoom: zoom + 0.5,
-              });
+        var clusterId = features[0].properties.cluster_id;
+        map
+          .getSource("point-source")
+          .getClusterExpansionZoom(clusterId, function (err, zoom) {
+            if (err) {
+              return;
+            }
+            map.easeTo({
+              center: features[0].geometry.coordinates,
+              zoom: zoom + 0.5,
             });
-        });
-        map.on("mouseenter", "clusters", function () {
-          map.getCanvas().style.cursor = "pointer";
-        });
-        map.on("mouseleave", "clusters", function () {
-          map.getCanvas().style.cursor = "";
-        });
+          });
+      });
+      map.on("mouseenter", "clusters", function () {
+        map.getCanvas().style.cursor = "pointer";
+      });
+      map.on("mouseleave", "clusters", function () {
+        map.getCanvas().style.cursor = "";
       });
     },
   },
   mounted() {
     this.inzializeMap();
-    this.createMarkers();
   },
   watch: {
     propApartments: {
