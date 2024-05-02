c5af39ee3b8b0843b6e9026617a0d6dfd48a2930
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-05-02 13:17:52 +0200

    fix: footer styling and responsiveness

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -11,98 +11,72 @@ export default {
 </script>
 
 <template>
-  <footer class="text-center text-lg-start text-light bg-dark">
-    <section class="d-flex flex-column align-items-center p-4 border-bottom">
-      <div
-        class="d-flex justify-content-between col-8 gap-3 my-2 pb-4 border-bottom"
-      >
+<footer class="text-center text-lg-start text-light bg-dark">
+
+  <section class="p-3 d-flex flex-wrap justify-content-center justify-content-lg-between align-items-center border-bottom flex-column">
+    
+    <div class="col-8 gap-3 pb-2 border-bottom">
+
+      <!-- Social icons -->
+      <div class="d-flex gap-5 justify-content-center">
+        <a href="#" class="text-light text-decoration-none"><i class="fab fa-facebook-f fs-4"></i></a>
+        <a href="#" class="text-light text-decoration-none"><i class="fab fa-twitter fs-4"></i></a>
+        <a href="#" class="text-light text-decoration-none"><i class="fab fa-instagram fs-4"></i></a>
+        <a href="#" class="text-light text-decoration-none"><i class="fab fa-telegram-plane fs-4"></i></a>
+      </div>
+
+    </div>
+
+    
+    <div class="container pt-3">
+      
+      <!-- Language and currency selectors -->
+      <div class="d-flex flex-row align-items-center justify-content-around justify-content-md-center gap-md-5">
+
+        <!-- Language selector -->
+        <div class="dropdown">
+          <a class="text-light dropdown-toggle text-decoration-none" href="#" role="button" id="dropdownLanguageLink" data-bs-toggle="dropdown" aria-expanded="false">
+            Italiano (IT)
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="dropdownLanguageLink">
+            <li><a class="dropdown-item text-decoration-none" href="#">English (EN)</a></li>
+            <li><a class="dropdown-item text-decoration-none" href="#">Español (ES)</a></li>
+          </ul>
+        </div>
+
+        <!-- Currency selector -->
+        <div class="dropdown">
+          <a class="text-light dropdown-toggle text-decoration-none" href="#" role="button" id="dropdownCurrencyLink" data-bs-toggle="dropdown" aria-expanded="false">
+            € EUR
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="dropdownCurrencyLink">
+            <li><a class="dropdown-item text-decoration-none" href="#">$ USD</a></li>
+            <li><a class="dropdown-item text-decoration-none" href="#">£ GBP</a></li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div class="d-flex flex-row align-items-center justify-content-around">
         <!-- Links -->
-        <div>
-          <a href="#" class="text-light me-4 text-decoration-none">Privacy</a>
-          <a href="#" class="text-light me-4 text-decoration-none">Policy</a>
-          <a href="#" class="text-light me-4 text-decoration-none"
-            >Mappa del sito</a
-          >
-          <a href="#" class="text-light me-4 text-decoration-none"
-            >Agency's Details</a
-          >
-        </div>
-
-        <!-- Language and currency selectors -->
-        <div class="d-flex justify-content-center">
-          <!-- Language selector -->
-          <div class="dropdown me-4 text-decoration-none">
-            <a
-              class="text-light dropdown-toggle text-decoration-none"
-              href="#"
-              role="button"
-              id="dropdownLanguageLink"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Italiano (IT)
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="dropdownLanguageLink">
-              <li>
-                <a class="dropdown-item text-decoration-none" href="#">
-                  English (EN)</a
-                >
-              </li>
-              <li>
-                <a class="dropdown-item text-decoration-none" href="#"
-                  >Español (ES)</a
-                >
-              </li>
-              <!-- Add other languages here -->
-            </ul>
-          </div>
-          <!-- Currency selector -->
-          <div class="dropdown">
-            <a
-              class="text-light dropdown-toggle text-decoration-none"
-              href="#"
-              role="button"
-              id="dropdownCurrencyLink"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              € EUR
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="dropdownCurrencyLink">
-              <li>
-                <a class="dropdown-item text-decoration-none" href="#">$ USD</a>
-              </li>
-              <li>
-                <a class="dropdown-item text-decoration-none" href="#">£ GBP</a>
-              </li>
-              <!-- Add other currencies here -->
-            </ul>
-          </div>
-        </div>
-
-        <!-- Social icons -->
-        <div class="">
-          <a href="#" class="text-light me-4 text-decoration-none">
-            <i class="fab fa-facebook-f fs-4"></i>
-          </a>
-          <a href="#" class="text-light me-4 text-decoration-none">
-            <i class="fab fa-twitter fs-4"></i>
-          </a>
-          <a href="#" class="text-light me-4 text-decoration-none">
-            <i class="fab fa-instagram fs-4"></i>
-          </a>
-          <a href="#" class="text-light me-4 text-decoration-none">
-            <i class="fab fa-telegram-plane fs-4"></i>
-          </a>
+        <div class="d-flex flex-row flex-lg-row gap-2 mt-3">
+          <a href="#" class="text-light mx-1 text-decoration-underline">Privacy</a>
+          <a href="#" class="text-light mx-1 text-decoration-underline">Policy</a>
+          <a href="#" class="text-light mx-1 text-decoration-underline">Mappa del sito</a>
+          <a href="#" class="text-light mx-1 text-decoration-underline">Agency's Details</a>
         </div>
       </div>
 
-      <!-- Brand -->
-      <div class="my-2">
-        <span>&copy; 2024 BoolBnB</span>
-      </div>
-    </section>
-  </footer>
+    </div>
+
+  </section>
+
+  <!-- Brand -->
+  <div class="my-2 text-center py-2">
+      <span>&copy; 2024 BoolBnB</span>
+  </div>
+</footer>
+
 </template>
 
 <style lang="scss">
