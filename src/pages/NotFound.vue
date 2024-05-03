9c6bd24a110f83cdc11305414b8a926e72c64838
Anna <annadalex@icloud.com>
2024-05-03 12:00:37 +0200

    change page not-found

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,15 +1,20 @@
-<template>
-    <div>
-      <h1>404 - Pagina non trovata</h1>
-      <p>La pagina che hai cercato non esiste.</p>
-    </div>
-  </template>
-  
-  <script>
+<script>
   export default {
     name: 'NotFound'
   }
   </script>
+  <template>
+    <div class="container">
+  <div class="row">
+    <div class="col-md-6 offset-md-3">
+        <h1 class="text-center">404 - Page Not Found</h1>
+        <p class="text-center">The page you requested could not be found.</p>
+    </div>
+  </div>
+</div>
+  </template>
+  
+  
   
   <style scoped>
  
