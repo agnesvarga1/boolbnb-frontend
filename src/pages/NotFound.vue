6356c1b80b574d75a3f47254b2842e1861c31335
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-05-03 12:43:32 +0200

    fix: error 404 display

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,21 +1,50 @@
 <script>
   export default {
-    name: 'NotFound'
+    name: 'NotFound',
+  methods:{
+    hideButton() {
+      const upButton = document.getElementById("upButton");
+      const upContainer = document.getElementById("upContainer");
+      upButton.style.display = "none";
+      upContainer.classList.remove("mb-4");
+    },
+    resetStylesAndClasses() {
+      const upButton = document.getElementById("upButton");
+      const upContainer = document.getElementById("upContainer");
+      upButton.style.display = "block";
+      upContainer.classList.add("mb-4");
+    },
+  },
+    mounted() {
+    this.hideButton();
+    },
+    beforeRouteLeave(to, from, next) {
+      this.resetStylesAndClasses();
+      next();
+    },
+  
   }
   </script>
   <template>
-    <div class="container">
-  <div class="row">
-    <div class="col-md-6 offset-md-3">
-        <h1 class="text-center">404 - Page Not Found</h1>
-        <p class="text-center">The page you requested could not be found.</p>
+    <div class="container d-flex align-items-center justify-content-center">
+      <div class="row ">
+        <div class="mt-3">
+            <h2 class="text-center fw-bold m-0 p-0">Errore</h2>
+            <h1 class="text-center fw-bold error m-0 p-0">404</h1>
+            <h5 class="text-center mt-0">Pagina non trovata</h5>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   </template>
   
   
   
   <style scoped>
- 
+.container{
+  min-height: 80vh;
+  width: 100%;
+}
+.error{
+  font-size: 80px;
+}
   </style>
--- src/router.js
+++ src/router.js
@@ -23,7 +23,7 @@ const router = createRouter({
       component: SingleApartment,
     },
     {
-      path: '*',
+      path: '/:pathMatch(.*)*',
       component: NotFound
     },
   ],
